1a9a023e5ba66118c09b859d0d0f5a8c3a8dcee1
Tien Tran <tientran@Tiens-MacBook-Pro.local>
2023-06-13 21:03:33 -0700

    Init firebase

--- wl_lead_generator/lib/main.dart
+++ wl_lead_generator/lib/main.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:solana_wallet_adapter/solana_wallet_adapter.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'firebase_options.dart';
 import 'views/landing_page.dart';
 
-void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // SolanaWalletAdapterPlatform.instance.setProvider(MyApp)
   //await SolanaWalletAdapter.initialize();
 
   SolanaWalletAdapterPlatform.instance.setProvider(AppInfo.phantom);
-
+  // FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  // var console;
+  // console.log(analytics);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
 
   // final AppInfo appInfo = const AppInfo(
   //   name: 'My App',
